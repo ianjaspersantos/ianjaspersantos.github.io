9e403fbbfb7b0dcce7ac75e041bcb8d9a241f58b
Ian Jasper Santos <57083058+ianjaspersantos@users.noreply.github.com>
2020-05-24 17:37:56 +0800

    Add files via upload

---
+++ grab-food-clone/flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "index.html": "141773b19d751f7fd1033b28e6784daa",
+"/": "141773b19d751f7fd1033b28e6784daa",
+"main.dart.js": "51c9a3a36f8a8631a405e1752eaa44d6",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "3534c59b376fc115c30b6f8ee999557c",
+"assets/LICENSE": "2909ef0d49d0f4d9bb78eff192ef95ee",
+"assets/AssetManifest.json": "251285d64fc60adf50ec33a2c3f4d624",
+"assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/images/error.png": "507df9de3fe43448ad4838afe237211b"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
